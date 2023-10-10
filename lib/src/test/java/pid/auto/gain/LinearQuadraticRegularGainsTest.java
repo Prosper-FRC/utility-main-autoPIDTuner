1b555696999aec2d416849a80670323441d209a2
Cody W. <CodyW2125@outlook.com>
2023-10-10 12:22:28 -0500

    Initial Commit

---
+++ lib/src/test/java/pid/auto/gain/LinearQuadraticRegularGainsTest.java
@@ -0,0 +1,18 @@
+// ----------------------------------------------------------------[Package]----------------------------------------------------------------//
+package pid.auto.gain;
+// ---------------------------------------------------------------[Libraries]---------------------------------------------------------------//
+
+// ---------------------------------------------------[Linear Quadratic Regular Gains Test]-------------------------------------------------//
+/**
+ * 
+ * 
+ * <h1> LinearQuadraticRegulatorGainsTest </h1>
+ *
+ * <p> </p>
+ *
+ * @author Cody Washington (@Jelatinone)
+ * @see LinearQuadraticRegularGains
+ */
+public class LinearQuadraticRegularGainsTest {
+  
+}
